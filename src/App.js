786738882853a8f98bae37e32d59d1a7b95560fb
YugenFox <kalebsday@gmail.com>
2023-04-05 22:08:40 -0500

    Initial commit - finished UI checkbox

--- src/App.js
+++ src/App.js
@@ -1,22 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import SendIcon from "@mui/icons-material/Send";
+import CancelScheduleSendIcon from "@mui/icons-material/CancelScheduleSend";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+
+function CheckboxExample() {
+  const [checked, setChecked] = React.useState(true);
+
+  return (
+    <FormControlLabel
+      control={
+        <Checkbox
+          checked={checked}
+          onChange={(e) => setChecked(e.target.checked)}
+          inputProps={{
+            "aria-label": "secondary checkbox",
+          }}
+          checkedIcon={<SendIcon />}
+          icon={<CancelScheduleSendIcon />}
+        />
+      }
+      label='happy label'
+    />
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <CheckboxExample />
+        <ButtonGroup variant="contained" color="primary">
+          <Button size="large" color="primary" startIcon={<SendIcon />}>
+            Send
+          </Button>
+          <Button
+            size="large"
+            color="secondary"
+            startIcon={<CancelScheduleSendIcon />}
+          >
+            Unsend
+          </Button>
+        </ButtonGroup>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
